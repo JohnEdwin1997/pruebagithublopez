7c7e1300a5419061271eb92acbb9d6d7f77964b9
JohnEdwin1997 <114839955+JohnEdwin1997@users.noreply.github.com>
2024-04-28 12:35:05 -0600

    ConfirmacionLopez1
    Ok

---
+++ Sesion23LopezISGITHUB/src/sesion23lopezisgithub/Sesion23LopezISGITHUB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion23lopezisgithub;
+
+/**
+ *
+ * @author JohnE
+ */
+public class Sesion23LopezISGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Prueba de GITHUB 1 LOPEZ");
+                
+    }
+    
+}
